9c6026b5bac257746906db3ef87d816364aa1138
Nathan Fallowfield <nfallowf@uwo.ca>
2020-08-16 12:02:39 -0400

    merged searchresults feature

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*This is the header section, perhaps search here?*/}
+
       </header>
+      {/*}
       <section className="Main-Content">
-        {/*This is where search results will be diplayed*/}
+        {/*This is where search results will be diplayed}
         <SearchResults/>
       </section>
+      {*/}
     </div>
   );
 }
